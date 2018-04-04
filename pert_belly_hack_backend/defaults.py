37d8a2762a93af394aacbd6ad6e0569504fb46a4
doubleO8 <wb008@hdm-stuttgart.de>
2018-04-04 20:28:16 +0200

    package name, epoch updated

--- pert_belly_hack_backend/defaults.py
+++ pert_belly_hack_backend/defaults.py
@@ -4,8 +4,8 @@ from . import __version__
 
 #: meta data for opkg
 PACKAGE_META = {
-    "package": "enigma2-plugin-extensions-openwebif",
+    "package": "pert-belly-hack-backend",
     "upstream_version": __version__,
-    "epoch": 1,
+    "epoch": 2,
     "target_root_path": "OpenWebif"
 }
