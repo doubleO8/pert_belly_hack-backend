3d0dfa2155608b9876e98384571d2557c97323b9
doubleO8 <wb008@hdm-stuttgart.de>
2018-04-05 20:05:17 +0200

    updated README and packaging meta data

--- pert_belly_hack_backend/defaults.py
+++ pert_belly_hack_backend/defaults.py
@@ -7,5 +7,7 @@ PACKAGE_META = {
     "package": "pert-belly-hack-backend",
     "upstream_version": __version__,
     "epoch": 2,
-    "target_root_path": "OpenWebif"
+    "target_root_path": "OpenWebif",
+    "description": "backend component providing XML and RESTful APIs",
+    "conflicts": "enigma2-plugin-extensions-openwebif",
 }
